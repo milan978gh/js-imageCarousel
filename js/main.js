91202dbc770c1a5189e922b1b90eeac77993b7ac
milan978gh <amilandev78@gmail.com>
2020-12-25 15:19:18 +0100

    updated css, js

---
+++ js/main.js
@@ -0,0 +1,29 @@
+const gallery = [
+  {
+    id: 1,
+    name: '1',
+    img: '../img/img1.jpg'
+  },
+  {
+    id: 2,
+    name: '2',
+    img: '../img/img2.jpg'
+  },
+  {
+    id: 3,
+    name: '3',
+    img: '../img/img3.jpg'
+  },
+  {
+    id: 4,
+    name: '4',
+    img: '../img/img4.jpg'
+  },
+  {
+    id: 5,
+    name: '5',
+    img: '../img/img5.jpg'
+  }
+];
+
+
