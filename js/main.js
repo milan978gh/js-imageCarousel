df46074e0a73d0e26dc4f7a13799babb630b22ec
milan978gh <amilandev78@gmail.com>
2020-12-27 19:29:35 +0100

    all done

--- js/main.js
+++ js/main.js
@@ -33,16 +33,26 @@ const nextBtn = document.querySelector('.next-btn');
 let currentItem = 0;
 
 window.addEventListener('DOMContentLoaded', () => {
-  loadItem(currentItem);
+  showItem(currentItem);
 });
 
 prevBtn.addEventListener('click', () => {
-
+  currentItem--;
+  if (currentItem < 0) {
+    currentItem = gallery.length - 1;
+  }
+  showItem(currentItem);
 });
 nextBtn.addEventListener('click', () => {
-
+  currentItem++;
+  if (currentItem > gallery.length - 1) {
+    currentItem = 0;
+  }
+  showItem(currentItem);
 });
 
-function loadItem(){
-  
+function showItem(item) {
+  const image = gallery[item];
+  slideImg.src = image.img;
+  slideInfo.textContent = image.name;
 }
